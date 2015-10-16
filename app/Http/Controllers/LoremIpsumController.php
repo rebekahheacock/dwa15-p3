e2d3d2ac466fdd8da10c8df90f4ced89490fce7a
Rebekah Heacock Jones <rebekah.heacock@gmail.com>
2015-10-16 15:19:48 -0400

    get Lorem Ipsum generator working

--- app/Http/Controllers/LoremIpsumController.php
+++ app/Http/Controllers/LoremIpsumController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
+use Illuminate\Http\Request;
+
 class LoremIpsumController extends Controller {
 
     public function __construct() {
@@ -11,17 +13,21 @@ class LoremIpsumController extends Controller {
     }
 
     /**
-    * Responds to requests to GET /lorempipsum
+    * Responds to requests to GET /loremipsum
     */
     public function getIndex() {
-
         return view('loremipsum');
     }
 
     /**
-     * Responds to requests to POST /lorempipsum
+     * Responds to requests to POST /loremipsum
      */
-    public function postIndex() {
-        return 'Process form for Lorem Ipsum generator';
+    public function postIndex(Request $request) {
+        $grafs = $request->input('grafs');
+
+        $generator = new \Badcow\LoremIpsum\Generator();
+        $paragraphs = $generator->getParagraphs($grafs);
+
+        return view('loremipsum')->with('paragraphs', $paragraphs);
     }
 }
--- resources/views/loremipsum.blade.php
+++ resources/views/loremipsum.blade.php
@@ -9,10 +9,20 @@
 @stop
 
 @section('content')
-	<p>form of some sort, plus place to display results</p>
+	<form method='POST' action='loremipsum'>
+	    <input type='hidden' name='_token' value='{{ csrf_token() }}'>
+	    <div class="form-group">
+		    <label for="grafs">Number of paragraphs (max 10)</label>
+		    <input type="number" name="grafs" min="1" max="10">
+		</div>
+		<button type="submit" class="btn btn-default" title="Let's do it">Faciamus hoc</button>
+	</form>
 
-	@if(isset($litext))
-        <p>Show paragraphs of text</p>
+	@if(isset($paragraphs))
+		<div id="loremipsumtext">
+        	<p>
+        	<?php echo implode('<p>', $paragraphs); ?>
+        </div>
     @else
         <p>blank</p>
     @endif
