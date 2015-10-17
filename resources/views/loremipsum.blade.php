368f62093b866af30910c848d3308a47908ef713
Rebekah Heacock Jones <rebekah.heacock@gmail.com>
2015-10-17 12:44:50 -0400

    include HTML5 limits on grafs input

--- resources/views/loremipsum.blade.php
+++ resources/views/loremipsum.blade.php
@@ -9,6 +9,15 @@
 @stop
 
 @section('content')
+
+	@if(count($errors) > 0)
+	    <ul>
+	        @foreach ($errors->all() as $error)
+	            <li>{{ $error }}</li>
+	        @endforeach
+	    </ul>
+	@endif
+
 	<form method='POST' action='loremipsum'>
 	    <input type='hidden' name='_token' value='{{ csrf_token() }}'>
 	    <div class="form-group">
